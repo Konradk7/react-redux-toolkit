821b64cabba70b45f894f0a77bffb7b3c8214d58
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-08-11 19:25:18 +0200

    add actions creator

---
+++ src/redux/actionCreator.js
@@ -0,0 +1,20 @@
+import {v4} from 'uuid';
+
+const addTodo = (message) => ({
+    type: TODOS_ACTIONS_TYPES.ADD,
+    payload: {id: v4(), message}
+});
+
+const deleteTodo = (id) => ({
+    type: TODOS_ACTIONS_TYPES.DELETE,
+    payload: id
+});
+
+const updateTodo = (id, message) => ({
+    type: TODOS_ACTIONS_TYPES.UPDATE,
+    payload: { id, message}
+});
+
+const removeAllTodos = () => ({
+    type: TODOS_ACTIONS_TYPES.REMOVE_ALL
+})
