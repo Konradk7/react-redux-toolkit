2c0da1530fc332ae6959a89d8192d5c8ed842e23
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-08-11 19:33:33 +0200

    add reducer

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -4,3 +4,31 @@ const defaultState = {
     todos: [],
     example: { isChanged: false},
 }
+
+const reducer = (state = defaultState, action) => {
+    switch (action.type) {
+        case TODOS_ACTIONS_TYPES.ADD:
+            return {...state, todos: [...state.todos, action.payload]};
+        case TODOS_ACTIONS_TYPES.DELETE:
+            const filteredTodos = state.todos.filter(
+                ({id}) => id !== action.payload)
+            ;
+            return {...state, todos: filteredTodos };
+        case TODOS_ACTIONS_TYPES.UPDATE:
+            const todos = state.todos.map(todo => {
+                if (todo.id === action.payload.id) {
+                    return {
+                        ...todo,
+                        message: action.payload.message
+                    };
+                }
+                return todo;
+            })
+            return {...state, todos };
+        case TODOS_ACTIONS_TYPES.REMOVE_ALL:
+            return {...state, todos: []};
+        default:
+            return state;
+    }
+
+}
