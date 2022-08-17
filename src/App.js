ecd7eaa44b95c64e7b1f4c614a096f755f513782
Konrad Kędroń <konrad.kdro7@gmail.com>
2022-08-17 19:41:49 +0200

    Imports

--- src/App.js
+++ src/App.js
@@ -1,17 +1,36 @@
 import React from 'react';
 import './sass/App.scss';
+import {useSelector} from "react-redux";
+import {useDispatch} from "react-redux";
+import  addTodo from "./redux/actionCreator";
+import deleteTodo from "./redux/actionCreator";
 
 function App() {
+    const dispatch = useDispatch();
+    const todos = useSelector((state) => state.todos);
+
+
+
     return (
         <div className="App">
             <div className="app_container">
                 <h1 className="app_container--title">Todos tasks</h1>
                 <div className="app_container--add-todo">
-                    <input type="text" placeholder="Write your task"/>
+                    <input type="text" placeholder="Write your task" onChange={() => dispatch(addTodo)}/>
                     <button>Add Todo</button>
                 </div>
                 <div className="app_container--todos">
                     <ul>
+                        {todos.map((todo) => (
+                            <li key={todo.id}>
+                                <h3 >Task</h3>
+                                <span>{todo.title}</span>
+                                <div className="buttons">
+                                    <button className="buttons_delete-btn">Delete</button>
+                                    <button className="buttons_done-btn">Done</button>
+                                </div>
+                            </li>
+                        ))}
                         <li>
                             <h3>Task 1</h3>
                             <span>Make a cake</span>
